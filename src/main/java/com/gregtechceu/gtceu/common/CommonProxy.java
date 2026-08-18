32a54983f0de8067755d8bf4bc2d5f8de2a37c59
Gustavo <ofoxsmith@outlook.com>
2026-08-18 14:08:12 +1000

    update pattern errors

--- src/main/java/com/gregtechceu/gtceu/common/CommonProxy.java
+++ src/main/java/com/gregtechceu/gtceu/common/CommonProxy.java
@@ -195,7 +195,7 @@ public class CommonProxy {
         ChanceLogic.init();
         GTRecipeTypes.init();
         GTRecipeCategories.init();
-        GTPatternErrors.init();
+        GTPatternErrors.init(modBus);
 
         GTFoods.init();
         GTToolTiers.init();
